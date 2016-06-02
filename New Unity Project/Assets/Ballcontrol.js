2ec9918bba43ff772bde535c3bcad722206dd03e
KEVIN <baranyizigiyekevin@gmail.com>
2016-06-02 14:38:47 +0530

    Added the jump function

--- New Unity Project/Assets/Ballcontrol.js
+++ New Unity Project/Assets/Ballcontrol.js
@@ -2,9 +2,14 @@
 
 
 var rotationSpeed = 100 ;
+var jumpHeight = 8 ;
+
+private var isFalling = false ; // do not appear in Unity
+
 
 function Update () {
 
+//Handle Ball Rotation
 var rotation : float = Input.GetAxis ("Horizontal") * rotationSpeed ;
 
 rotation *= Time.deltaTime ;
@@ -12,5 +17,17 @@ rotation *= Time.deltaTime ;
 GetComponent.<Rigidbody>().AddRelativeTorque(Vector3.back * rotation) ;
 
 
+if (Input.GetKeyDown(KeyCode.W) && isFalling == false)
+{
+
+GetComponent.<Rigidbody>().velocity.y =jumpHeight;
+
+}
+isFalling = true ;
+}
+
+function OnCollisionStay (){
+
+  isFalling = false;
 
 }
