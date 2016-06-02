4d8dec4ffa7dae14642383932f2a3ffcce0f429c
KEVIN <baranyizigiyekevin@gmail.com>
2016-06-02 13:54:17 +0530

    First Commit

---
+++ New Unity Project/Assets/Ballcontrol.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+
+var rotationSpeed = 100 ;
+
+function Update () {
+
+var rotation : float = Input.GetAxis ("Horizontal") * rotationSpeed ;
+
+rotation *= Time.deltaTime ;
+
+GetComponent.<Rigidbody>().AddRelativeTorque(Vector3.back * rotation) ;
+
+
+
+}
